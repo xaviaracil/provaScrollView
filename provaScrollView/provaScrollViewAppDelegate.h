02e1f277cbc439fbcba29d0a572684758cd71f41
Xavi Aracil <xaviaracil@me.com>
2011-05-11 21:30:38 +0200

    Initial Commit

---
+++ provaScrollView/provaScrollViewAppDelegate.h
@@ -0,0 +1,21 @@
+//
+//  provaScrollViewAppDelegate.h
+//  provaScrollView
+//
+//  Created by Xavi Aracil on 11/05/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class provaScrollViewViewController;
+
+@interface provaScrollViewAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@property (nonatomic, retain) IBOutlet provaScrollViewViewController *viewController;
+
+@end
---
+++ provaScrollViewTests/provaScrollViewTests.m
@@ -0,0 +1,33 @@
+//
+//  provaScrollViewTests.m
+//  provaScrollViewTests
+//
+//  Created by Xavi Aracil on 11/05/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "provaScrollViewTests.h"
+
+
+@implementation provaScrollViewTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in provaScrollViewTests");
+}
+
+@end
